117d0a98995f691de94deca62ef0f71cd7fa22d1
RousseauMalco <rousseaumalco@gmail.com>
2024-10-14 11:01:03 -0500

    Working on loopless pt 3.

--- lib/loopless.rb
+++ lib/loopless.rb
@@ -12,34 +12,15 @@ module GoingLoopless
   # chronological order.
   #
   def list_movies(person)
-    movies = []
-    person.roles.each do |role|
-      unless movies.include?(role.movie)
-        movies << role.movie
-      end
-    end
-    movies.sort_by!(&:year)  #  (&:year) is shorthand for { |o| o.year }
-
-    results = []
-    movies.each do |movie|
-      results << "#{movie.title} (#{movie.year})"
-    end
-    results
+    person.roles.map(&:movie).uniq.sort_by!(&:year).map { |movie| "#{movie.title} (#{movie.year})" }
   end
 
   # Creates a list of credits entry of the form "Person Name (role)", with the roles appearing in
   # the order specified in role_order, and each person appearing multiple times if they took on
-  # multiple roles in the film.
+  # multiple roles in the film. 
   #
-  def build_credits(movie, role_order)
-    results = []
-    role_order.each do |role_name|
-      movie.roles.each do |role|
-        if role.name == role_name
-          results << "#{role.person.name} (#{role.name})"
-        end
-      end
-    end
-    results
+  def build_credits(movie, role_order) # ["director", "writer", "actor", "key grip"]
+    role_order.map { |role_name| movie.roles.select { |role| role.name.include?(role_name) }.map 
+        { |entry| "#{entry.person.name} (#{entry.name})"  } }
   end
 end
