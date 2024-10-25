e484045756ad775e16622a9d8b3a30e53a61b165
LucyManalang <lucemanalang@gmail.com>
2024-10-25 13:50:28 -0500

    Done for now, may do pt3 later

--- lib/loopless.rb
+++ lib/loopless.rb
@@ -5,35 +5,20 @@ module GoingLoopless
   # Returns all the people who have taken on the role with the given name.
   #
   def find_all_in_role(role_name, people)
-    results = []
-    people.each do |person|
-      person.roles.each do |role|
-        if role.name == role_name
-          results << person
-          break
-        end
+    people.select do |person| 
+      person.roles.any? do |role| 
+        role.name == role_name 
       end
     end
-    results
   end
 
   # List the titles and years of all the movies in which the given person played a role, in
   # chronological order.
   #
   def list_movies(person)
-    movies = []
-    person.roles.each do |role|
-      unless movies.include?(role.movie)
-        movies << role.movie
-      end
+    person.roles.map { |role| role.movie }.sort_by(&:year).uniq.map do 
+      |movie| "#{movie.title} (#{movie.year})" 
     end
-    movies.sort_by!(&:year)  #  (&:year) is shorthand for { |o| o.year }
-
-    results = []
-    movies.each do |movie|
-      results << "#{movie.title} (#{movie.year})"
-    end
-    results
   end
 
   # Creates a list of credits entry of the form "Person Name (role)", with the roles appearing in
@@ -41,14 +26,10 @@ module GoingLoopless
   # multiple roles in the film.
   #
   def build_credits(movie, role_order)
-    results = []
-    role_order.each do |role_name|
-      movie.roles.each do |role|
-        if role.name == role_name
-          results << "#{role.person.name} (#{role.name})"
-        end
+    role_order.flat_map do |role_name|
+      movie.roles.select { |role| role.name == role_name }.map do |role|
+        "#{role.person.name} (#{role.name})"
       end
     end
-    results
   end
 end
